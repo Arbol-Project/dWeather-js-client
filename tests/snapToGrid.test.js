d8128b0a6dcf3b86aa0da2d1ce3e3d01803b5c5e
Ben Andre <bandre@gmail.com>
2020-10-21 21:53:45 -0400

    Add another test.

--- tests/snapToGrid.test.js
+++ tests/snapToGrid.test.js
@@ -3,9 +3,29 @@ const utils = require('../src/utils.js')
 
 test('snaps a coordinate pair for chirps_05-daily', () => {
   ipfsClient.getHeads().then(async (heads) => {
-  	dataset = heads['chirps_05-daily']
+    dataset = heads['chirps_05-daily']
     ipfsClient.getMetadata(dataset).then((metadata) => {
       expect(utils.snapToGrid(38, -68, metadata)).toBe(['37.975', '-68.025'])
-	}).catch(function () { ; } );
+    }).catch(function () { ; } );
+  });
+});
+
+
+test('snaps a coordinate pair for prism_rev_3_precip-daily', () => {
+  ipfsClient.getHeads().then(async (heads) => {
+    dataset = heads['prism_rev_3_precip-daily']
+    ipfsClient.getMetadata(dataset).then((metadata) => {
+      expect(utils.snapToGrid(39.2854944, -74.568208, metadata)).toBe(['39.292', '-74.583'])
+    }).catch(function () { ; } );
+  });
+});
+
+
+test('snaps a coordinate pair for prism_rev_3_precip-daily', () => {
+  ipfsClient.getHeads().then(async (heads) => {
+    dataset = heads['prism_rev_3_precip-daily']
+    ipfsClient.getMetadata(dataset).then((metadata) => {
+      expect(utils.snapToGrid(39.2744939 -74.5817372, metadata)).toBe(['39.292', '-74.583'])
+    }).catch(function () { ; } );
   });
 });
